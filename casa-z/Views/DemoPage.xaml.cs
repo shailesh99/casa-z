b5b9b2c4b0711a1580175d85f21cc0338013238b
shailesh99 <shailesh99@gmail.com>
2016-02-01 09:04:08 -0600

    Demo View, very very very basic

---
+++ casa-z/Views/DemoPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace casaz
+{
+	public partial class DemoPage : ContentPage
+	{
+		public DemoPage ()
+		{
+			InitializeComponent ();
+		}
+
+		private void StartButton_OnClicked(object sender, EventArgs e)
+		{
+			var name = StartEntry.Text;
+			var greeting = "Hello " + name;
+			StartLabel.Text = greeting;
+		}
+	}
+}
+
