8e399b245136aeb9b5e03b6bbe1d91cc5210cfcb
shailesh99 <shailesh99@gmail.com>
2016-02-08 06:27:42 -0600

    Testing branch

---
+++ casa-z/Views/ScanView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace casaz
+{
+	public partial class ScanView : ContentPage
+	{
+		public ScanView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
